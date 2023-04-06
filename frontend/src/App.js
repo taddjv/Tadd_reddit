22d231600b600b79b888665b8cd03774bd776c32
Tadd JV <taddjv111@gmail.com>
2023-04-06 17:46:08 -0400

    reset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,7 @@
-import React, { useState, useEffect } from "react";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { Route, Switch, useHistory } from "react-router-dom";
-import * as usersActions from "./store/users";
-import * as subscriptionsActions from "./store/subscriptions";
-import * as votesActions from "./store/votes";
-import { usePop } from "./context/UserPopcontext";
-import { checkMode } from "./helper";
-
-import NavBar from "./components/NavBar";
-import Sidebar from "./components/Sidebar";
-import Feed from "./components/Feed";
-import Community from "./components/Community";
-import SelectedPost from "./components/Feed/SelectedPost";
-import SubmitPost from "./components/SubmitPost.js";
-import SearchResults from "./components/SearchResults/index";
-import User from "./components/User";
 
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const { setDropUser } = usePop();
-  useEffect(() => {
-    checkMode();
-
-    dispatch(usersActions.restoreTheUser()).then(async (res) => {
-      const data = await res;
-      dispatch(subscriptionsActions.getTheCommunitiesS(data._id));
-      dispatch(votesActions.getTheUserVotes(data._id));
-      setIsLoaded(true);
-    });
-  }, [dispatch]);
-
-  return (
-    <>
-      <div
-        onClick={() => {
-          setDropUser(false);
-        }}
-        className="App"
-      >
-        <NavBar isLoaded={isLoaded} />
-        <Switch>
-          <Route exact path="/post/:id">
-            <SelectedPost />
-          </Route>
-          <Route exact path="/">
-            <Sidebar />
-            <Feed type="home" />
-          </Route>
-          <Route exact path="/all">
-            <Sidebar />
-            <Feed type="all" />
-          </Route>
-          <Route exact path="/r/:communityName">
-            <Community />
-          </Route>
-          <Route exact path="/u/:username">
-            <Sidebar />
-            <User />
-          </Route>
-          <Route exact path="/search/:search">
-            <Sidebar />
-            <SearchResults />
-          </Route>
-          <Route exact path="/r/:communityName/submit">
-            <SubmitPost />
-          </Route>
-        </Switch>
-      </div>
-    </>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,35 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { Provider } from "react-redux";
-import { BrowserRouter, Route } from "react-router-dom";
-import UserPopProvider from "./context/UserPopcontext";
-import EditProvider from "./context/EditContext";
-import CommentProvider from "./context/CommentContext";
-import configureStore from "./store";
-import { restoreCSRF, csrfFetch } from "./store/csrf";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import * as usersActions from "./store/users";
-
-const store = configureStore();
-
-if (process.env.NODE_ENV !== "production") {
-  restoreCSRF();
-
-  window.csrfFetch = csrfFetch;
-  window.store = store;
-  window.usersActions = usersActions;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <UserPopProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </UserPopProvider>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
